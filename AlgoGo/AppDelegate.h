826e0e8b98539deeba62fef8e61879062db37bc5
Luo Xianyang <luoxyx@gmail.com>
2016-09-14 10:45:42 +0800

    Initial Commit

---
+++ AlgoGo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  AlgoGo
+//
+//  Created by Luo Xianyang on 14/09/2016.
+//  Copyright © 2016 Axiom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
